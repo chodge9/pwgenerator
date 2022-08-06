1c03723e0f6775f3285ce89d2b3bfb9ec962d880
Chantal Hodge <chantal_h09@yahoo.com>
2022-08-06 14:20:05 -0500

    criteria for project added

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  console.log("Hey! You clicked the button!")
+
+// 1.prompt the user for the password criteris
+//  a- password length 8-28
+//  b- lowercase, uppercase, and special characters
+// 2. validate the input
+// 3. generate pw based on criteria
+
+
+// 4. display 'password' on the page
+  return "Generated password will go here!"
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
